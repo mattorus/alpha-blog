d018c5a202e722554745668b5b1f4c429c9a9b23
mattorus <mattorus@gmail.com>
2019-02-22 14:59:44 -0800

    Implement user authentication & session control.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
-  
+  before_action :set_user, only: [:edit, :update, :show]
+  before_action :require_same_user, only: [:edit, :update]
+
   def index
     @users = User.paginate(page: params[:page], per_page: 2)
   end
@@ -19,11 +21,9 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:success] = "#{@user.username} updated."
       redirect_to articles_path
@@ -33,8 +33,18 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
     @user_articles = @user.articles.paginate(page: params[:page], per_page: 2)
+  end
+
+  def set_user    
+    @user = User.find(params[:id])
+  end
+
+  def require_same_user
+    if current_user != @user
+      flash[:danger] = "That is not yours."
+      redirect_to root_path
+    end
   end
 
   private
@@ -42,6 +52,5 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
-
 
 end
