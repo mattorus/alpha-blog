c79ad8b0e4127e36e0ae5b7fbaa431febf860aae
mattorus <mattorus@gmail.com>
2019-02-24 11:48:13 -0800

    Add admin restriction to category creation; add test for this new admin restriction.

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -2,7 +2,12 @@ require 'test_helper'
 
 class CreateCategoriesTest < ActionDispatch::IntegrationTest
 
+  def setup    
+    setup_admin_user
+  end
+
   test "get new category form and create category" do
+    sign_in_admin_user
     get new_category_path
     assert_template 'categories/new'
     assert_difference 'Category.count', 1 do
@@ -17,6 +22,7 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
   end
 
   test "invalid category submission results in failure" do
+    sign_in_admin_user
     get new_category_path
     assert_template 'categories/new'
     assert_no_difference 'Category.count' do
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,4 +7,16 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+  def sign_in_as(user, password)
+    post login_path, params: {session: {email: user.email, password: password}}
+  end
+
+  def setup_admin_user
+    @admin_user = User.create(username: "john", email: "john@example.com", password: "password", admin: true)
+  end
+  
+  def sign_in_admin_user
+    sign_in_as(@admin_user, "password")
+  end
+
 end
